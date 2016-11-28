b29b3acbb1fd82abe71f8a3bd56c02cae76fec1f
0xHJK <HJKdev+GitHub@gmail.com>
2016-11-28 13:06:39 +0800

    简化命令

--- rock.sh
+++ rock.sh
@@ -1,88 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 # Author HJK 2016-11-21
 
-QUIET=
-VERBOSE=
-CMD=
 REPO='hjk/passgen'
 NAME='passgen'
 DIR=$(pwd)/code
-PACKAGE=
-
-usage()
-{
-    echo "Usage: `basename $0` COMMAND [-qv] [-r REPO] [-n NAME] [-d DIR] [PACKAGE]"
-    echo "Commands: build run"
-    exit 1
-}
-
-if [ $# -eq 0 ] || [[ $1 == 'build' ]]; then
-    CMD='build'
-elif [[ $1 == 'run' ]]; then
-    CMD='run'
-else
-    usage
-fi
-
-echo "start ${CMD}"
-
-shift 1
-
-while getopts :qvr:n:d: OPTION
-do
-    case $OPTION in
-        q)
-            QUIET=y
-            ;;
-        v)
-            VERBOSE=y
-            ;;
-        r)
-            REPO=$OPTARG
-            ;;
-        n)
-            NAME=$OPTARG
-            ;;
-        d)
-            DIR=$OPTARG
-            ;;
-        \?)
-            usage
-            ;;
-    esac
-done
+PORT_HOST=5000
+PORT_DOCKER=5000
 
 run()
 {
     if [[ `docker ps -a | awk '{print $NF}' | grep "^${NAME}$"` ]]; then
-        docker start -i ${NAME}
+        docker start ${NAME}
     else
-        docker run -it --name ${NAME} -v ${DIR}:/root/code -p 5000:5000 ${REPO} python3 ./code/main.py
+        docker run -d --name ${NAME} -v ${DIR}:/root/code -p ${PORT_HOST}:${PORT_DOCKER} ${REPO} python3 ./code/main.py
     fi
 }
 
-build()
-{
-    # echo "build func"
-    if [[ `docker images | awk '{print $1}' | grep "^${REPO}$"` ]]; then
-        run
-    else
-        # echo "building...."
-        for pkg in $@
-        do
-            # echo "RUN apt -y install ${pkg}"
-            echo "RUN apt-get -y install ${pkg} >> Dockerfile"
-        done
-        docker build -t ${REPO} .
-        run
-    fi
-}
 
-shift $(($OPTIND - 1))
-
-if [[ ${CMD} == 'build' ]]; then
-    build $@
-elif [[ ${CMD} == 'run' ]]; then
+if [[ `docker images | awk '{print $1}' | grep "^${REPO}$"` ]]; then
+    run
+else
+    docker build -t ${REPO} .
     run
 fi
-
